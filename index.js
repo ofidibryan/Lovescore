6904d3a505dfb222787c514dd860e0e0adc75df1
ofidibryan <143598389+ofidibryan@users.noreply.github.com>
2023-10-01 23:00:56 +0100

    initializing my project

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+// ("let's see if your love is srtong :-)");
+// var firstName = prompt("what is your name:");
+// var secondName = prompt ("what is your love's name:");
+// var percent= Math.random() ;
+// percent = Math.floor( percent*100) + 1;
+// alert("hey " + firstName + " your love score for " + secondName + " is: "+ percent + "%")
+// if (percent > 80)
+// {
+// alert ( firstName + " your love for " + secondName + " is like milk and honey, so sweet🥰💯👑💞");
+// }
+// if (percent < 40)
+// {
+// alert ("sorry " + firstName + " your love for " + secondName  + " fake😂💔");
+// }
+const random = document.querySelector(".random");
+let randm = document.querySelector(".randomnum");
+
